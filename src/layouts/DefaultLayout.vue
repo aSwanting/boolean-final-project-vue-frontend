1ee3021cc3875b240fa2692cdd6c845eb8d0d2bb
Nicola Tabai <nicola.tabai98@gmail.com>
2024-02-02 15:48:00 +0100

    Add - DefaultLayout layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <AppHeader />
+
+    <slot></slot>
+
+    <footer>
+
+    </footer>
+
+</template>
+<script>
+import AppHeader from '../components/AppHeader.vue'
+export default {
+    components: {
+        AppHeader
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
