21c1c761b842a3c7323933a07f43850ffead188c
Nicola Tabai <nicola.tabai98@gmail.com>
2024-02-03 01:35:20 +0100

    Fix - home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,31 @@
 <template>
-    <div>
-        <h1>Hello World</h1>
+    <DefaultLayout>
+        <div>
+            <h1>{{ title }}</h1>
+            <h3>{{ description }}</h3>
 
-    </div>
+        </div>
+    </DefaultLayout>
 </template>
 
 <script>
+import DefaultLayout from '../layouts/DefaultLayout.vue';
 export default {
+    components: {
+        DefaultLayout
+    },
+    data() {
+        return {
+            title: 'BoolBnB',
+            description: 'Questo è un sito bellissimo'
+        }
+    },
+    mounted() {
+        console.log('home page montata')
+    },
+    unmounted() {
+        console.log('unmount home page')
+    }
 
 }
 </script>
