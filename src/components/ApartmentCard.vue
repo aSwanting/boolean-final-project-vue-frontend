9788d67ca679af641d78ffe01ce57f1dd1665735
Nicola Tabai <nicola.tabai98@gmail.com>
2024-02-03 01:10:24 +0100

    Add - ApartmentCard and section components

---
+++ src/components/ApartmentCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card apartment-card">
+        <h3>{{ apartment.description }}</h3>
+        <figure>
+            <img :src="apartment.cover_image" alt="">
+        </figure>
+
+        <p v-if="apartment.user">
+            {{ apartment.user.first_name }}
+        </p>
+
+        <router-link 
+        :to="{ name: 'apartments.show', params: { slug: apartment.slug}}">
+            Vedi Appartamento
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        apartment: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+<style></style>
