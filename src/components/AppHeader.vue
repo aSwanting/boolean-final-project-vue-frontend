80ca55be1c649f635a936766291cd0369891ee81
ValerioCarbone <valeriocarbone10@gmail.com>
2024-02-05 12:14:45 +0100

    change - header anchor to backend

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,21 +1,35 @@
 <template>
-    <ul class="d-flex">
+    <ul class="d-flex nav-items">
         <li>
             <router-link :to="{ name: 'home' }">Home</router-link>
         </li>
         <li>
             <router-link :to="{ name: 'apartments.index' }">Apartments</router-link>
         </li>
+        <li>
+            <a v-bind:href="BACKEND_URL">User Area</a>
+        </li>
 
     </ul>
 </template>
     
 <script>
+export default {
+    data() {
+        return {
+            BACKEND_URL: 'http://127.0.0.1:8000/',
+        }
+    }
+}
 </script>
 <style lang="scss" scoped>
 .d-flex {
     display: flex;
-    justify-content: center;
-    column-gap: 260px;
+    justify-content: space-evenly;
+}
+
+.nav-items {
+    font-size: 20px;
+    padding: 12px 0;
 }
 </style>
