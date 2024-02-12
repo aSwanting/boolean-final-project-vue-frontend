efa08d98ae77a957c3d560058e5f83247453cd0e
ValerioCarbone <valeriocarbone10@gmail.com>
2024-02-12 16:42:05 +0100

    change searchResults loader + carousel

--- src/pages/apartments/SearchResults.vue
+++ src/pages/apartments/SearchResults.vue
@@ -1,36 +1,54 @@
 <template>
-    <div class="container">
-        <div class="card-wrapper">
-            <ApartmentCard class="apartment-card" v-for="apartment in apartments" :apartment="apartment"
-                @click="store.currentApartment = apartment">
-            </ApartmentCard>
+    <DefaultLayout>
+        <div v-if="apartments[0]" class="container">
+            <div class="card-wrapper">
+                <ApartmentCard class="apartment-card" v-for="apartment in apartments" :apartment="apartment">
+                    <Carousel :apartment="apartment" />
+                </ApartmentCard>
+            </div>
         </div>
-    </div>
+        <div v-else-if="showLoader == false" class="no-apt">
+            <p>There are no apartments with the selected values!</p>
+            <router-link class="btn-primary" :to="{ name: 'home' }">
+                Home
+            </router-link>
+        </div>
+        <Loading v-show="showLoader == true"></Loading>
+    </DefaultLayout>
 </template>
 
 <script>
 import store from '../../store'
 import axios from 'axios'
 import ApartmentCard from '../../components/ApartmentCard.vue'
+import Carousel from "../../components/Carousel.vue"
+import Loading from "../../components/Loading.vue"
+import DefaultLayout from '../../layouts/DefaultLayout.vue'
 export default {
     components: {
-        ApartmentCard
+        ApartmentCard,
+        Carousel,
+        Loading,
+        DefaultLayout
     },
     data() {
         return {
             store,
             apartments: [],
+            showLoader: '',
         }
     },
     methods: {
         fetchResults() {
             axios.get('http://127.0.0.1:8000/api/apartments/results/').then(res => {
-                this.apartments = [];
                 this.apartments = res.data.results;
-                console.log(res.data.results);
+
+                this.showLoader = false;
+
             });
         },
         async getApartments() {
+            this.showLoader = true;
             if (store.lat) {
                 let response;
                 this.data = {
@@ -49,14 +67,15 @@ export default {
                     `${store.BACKEND_URL}api/apartments`,
                     this.data
                 );
-                console.log(response.data.results);
-                this.apartments = response.data.results.apartments
+                this.apartments = response.data.results.apartments;
+
+                this.showLoader = false;
             } else {
                 this.fetchResults();
             }
         }
     },
-    created() {
+    beforeMount() {
         this.getApartments();
     }
 }
@@ -76,6 +95,20 @@ export default {
 
     .apartment-card {
         cursor: pointer;
+    }
+}
+
+.no-apt {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .btn-primary {
+        flex-shrink: 1;
     }
 }
 </style>
