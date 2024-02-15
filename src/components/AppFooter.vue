3126323f717cb8917a680d70de947848c3373b38
karimdib <karim.dib2001@gmail.com>
2024-02-15 14:24:44 +0100

    refactory-footer-frontend

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,36 +9,107 @@ export default {
 </script>
 
 <template >
-  <footer>
-    <div class="row gap-2 ">
-      <div class="col-4">
-        <ul>
-          <li>Chi Siamo</li>
-          <li>Contatti</li>
-          <li>Pubblica il tuo annuncio</li>
-          <li>Lavora con noi</li>
-        </ul>
-      </div>
-      <div class="col-4">
-        <ul>
-          <li>Termini di utilizzo</li>
-          <li>Normativa sulla privacy e sui cookie</li>
-          <li>Appartamenti vicino a me </li>
-          <li>Domande Frequenti</li>
-        </ul>
-      </div>
-      <div class="col-4">
-        <div>Seguici su</div>
-        <ul class="d-flex gap-3">
-          <li><font-awesome-icon :icon="['fab', 'facebook']" /></li>
-          <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
-          <li><font-awesome-icon :icon="['fab', 'github']" /></li>
-          <li><font-awesome-icon :icon="['fab', 'telegram']" /></li>
-        </ul>
+  <footer class="ff">
+    <div class="container">
+      <div class="row  justify-content-between flex-wrap  flex-md-nowrap">
+        <div class="col-md-4 col-6">
+          <ul>
+            <a href="#">
+              <li>Chi Siamo</li>
+            </a>
+            <a href="#">
+              <li>Contatti</li>
+            </a>
+            <a href="#">
+              <li>Pubblica il tuo annuncio</li>
+            </a>
+            <a href="#">
+              <li>Lavora con noi</li>
+            </a>
+          </ul>
+        </div>
+        <div class="col-md-4 col-6">
+          <ul>
+            <a href="#">
+              <li>Termini di utilizzo</li>
+            </a>
+            <a href="#">
+              <li>Normativa sulla privacy e sui cookie</li>
+            </a>
+            <a href="#">
+              <li>Appartamenti vicino a me </li>
+            </a>
+            <a href="#">
+              <li>Domande Frequenti</li>
+            </a>
+          </ul>
+        </div>
+        <div class="col-md-4 col-12  text-center icon-col ">
+          <ul>
+            <a href="#">
+              <li class="icon">Seguici su:</li>
+            </a>
+          </ul>
+          <ul class="d-flex gap-3  justify-content-center justify-content-md-between p-0">
+            <a href="#">
+              <li class="icon"><font-awesome-icon :icon="['fab', 'facebook']" /></li>
+            </a>
+            <a href="#">
+              <li class="icon"><font-awesome-icon :icon="['fab', 'instagram']" /></li>
+            </a>
+            <a href="#">
+              <li class="icon"><font-awesome-icon :icon="['fab', 'github']" /></li>
+            </a>
+            <a href="#">
+              <li class="icon"><font-awesome-icon :icon="['fab', 'telegram']" /></li>
+            </a>
+          </ul>
+        </div>
+        <!-- <div class="col-12">
+          <img class="city-image" src="/public/cityy.png" alt="">
+        </div> -->
       </div>
     </div>
   </footer>
 </template>
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
+
+.ff {
+  border-top: 1px solid $primary;
+  padding: 20px;
+  margin-top: 20px;
+  background-color: white;
+  font-size: 13px;
+  font-weight: 600;
+
+
+  ul {
+    padding-left: 0px;
+  }
+
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+
+    .row {
+      flex-wrap: nowrap;
+
+      a>li {
+        padding: 10px;
+
+        &:hover {
+          box-shadow: inset 0px 0px 5px rgba(30, 233, 149, 0.836);
+
+        }
+      }
+
+      .icon {
+        font-size: 16px;
+      }
+    }
+  }
+
+
+}
 </style>
