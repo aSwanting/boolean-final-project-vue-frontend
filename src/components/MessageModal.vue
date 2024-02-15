023eb440e178c3bb0ab337add472376a6c959ed6
Nicola Tabai <nicola.tabai98@gmail.com>
2024-02-15 18:44:37 +0100

    Add - modal component Fix - show with message modal

---
+++ src/components/MessageModal.vue
@@ -0,0 +1,213 @@
+<template>
+    <div class="modal-backdrop">
+        <header class="modal-header">
+            <slot name="header">
+                Header
+            </slot>
+            <button type="button" class="btn-close" @click="closeModal">
+                close
+            </button>
+        </header>
+        <section class="modal-body">
+            <slot name="body">
+                <form action="POST" class="message-form" @submit.prevent="submitForm" v-show="isFormActive">
+                    <ul class="inputs-container">
+                        <li>
+                            <label for="name" class="form-label">Your name:</label>
+                            <input type="text" class="form-control" v-model="sender" placeholder="sender" id="name">
+                        </li>
+                        <li>
+                            <label for="email" class="form-label">Your email:</label>
+                            <input type="email" class="form-control" v-model="email" placeholder="Email" id="email">
+                        </li>
+                        <li>
+                            <label for="subject" class="form-label">Subject:</label>
+                            <input type="text" class="form-control" v-model="subject" placeholder="subject" id="subject">
+                        </li>
+                        <li>
+                            <label for="message" class="form-label">Message:</label>
+                            <textarea type="text" id="message" class="form-control" v-model="message" placeholder="Write a message"></textarea>
+                        </li>
+                    </ul>
+
+
+
+
+                    <button type="submit" @click="messageSent">Send message</button>
+                </form>
+                <p v-show="messageSentSuccess">Your message has been sent!</p>
+            </slot>
+        </section>
+        <!-- <footer class="modal-footer">
+        
+        </footer> -->
+    </div>
+</template>
+<script>
+import axios from 'axios';
+import store from '../store';
+export default {
+    data() {
+        return {
+            store,
+            subject: '',
+            message: '',
+            sender: '',
+            email: '',
+            isFormActive: true,
+            messageSentSuccess: false,
+        };
+    },
+    props: {
+        items: {
+            type: Object,
+            required: true,
+        },
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close-modal')
+        },
+        async submitForm() {
+            const data = {
+
+                apartment_id: this.items.id,
+                subject: this.subject,
+                content: this.message,
+                sender: this.sender,
+                email: this.email,
+            }
+            try {
+                await axios.post(`${this.store.BACKEND_URL}api/apartments/messages`, data)
+                console.log(data)
+            } catch (error) {
+                console.error('errore durantel\'invio del modulo;', error);
+                console.log(data)
+            }
+        },
+        messageSent() {
+            this.messageSentSuccess = true
+            this.isFormActive = false
+        }
+    },
+    mounted() {
+        // console.log(this.submitForm)
+    }
+}
+</script>
+<style lang="scss" scoped>
+@use "../styles/partials/_messageModal.scss" as *;
+
+.modal-backdrop {
+    position: absolute;
+    top: 50%;
+
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    width: 50%;
+    height: 500px;
+    /* Larghezza della modale */
+    background-color: rgba(255, 255, 255, 0.842);
+    /* Sfondo trasparente */
+    z-index: 9999;
+    /* Assicura che la modale sia sopra gli altri elementi */
+    overflow: auto;
+    /* Abilita lo scrolling se la modale è più grande dello schermo */
+    border-radius: 10px;
+    /* Bordi arrotondati */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    /* Ombra */
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+
+    .modal-header,
+    .modal-footer {
+        padding: 15px;
+        display: flex;
+    }
+
+    .modal-header {
+        position: relative;
+        border-bottom: 1px solid;
+        background-color: white;
+        justify-content: space-between;
+
+        .btn-close {
+            position: relative;
+            top: 0;
+            right: 0;
+            color: red;
+            border: none;
+            font-size: 20px;
+            padding: 10px;
+            cursor: pointer;
+            font-weight: bold;
+        }
+    }
+
+    .modal-footer {
+        border-top: 1px solid;
+        flex-direction: column;
+        justify-content: flex-end;
+    }
+
+    .modal-body {
+        position: relative;
+        padding: 20px 10px;
+        flex-grow: 1;
+        .message-form {
+
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            
+            .inputs-container {
+                flex-grow: 1;
+                display: flex;
+                flex-direction: column;
+            }
+        }
+        .form-control {
+            display: block;
+            width: 100%;
+            padding: 0.375rem 0.75rem;
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            appearance: none;
+            background-color: white;
+            background-clip: padding-box;
+            border: 1px solid black;
+            border-radius: 20px;
+            box-shadow: 4px rgba(0, 0, 0, 0.712);
+            transition-duration: 1500ms, 1500ms;
+            transition-timing-function: ease-in-out, ease-in-out;
+            transition-delay: 0, 0;
+            transition-property: box-shadow;
+        }
+        #message {
+            height: 80%;
+            overflow-y: auto;
+            resize: none;
+        }
+
+        .btn-send {
+            color: green;
+            border: none;
+            border-radius: 4px;
+        }
+    }
+}
+
+.modal-fade-enter,
+.modal-fade-leave {
+    opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+    transition: opacity 5s ease;
+}
+</style>
--- src/pages/apartments/Show.vue
+++ src/pages/apartments/Show.vue
@@ -1,17 +1,40 @@
 <template>
   <DefaultLayout>
-    <div class="container" v-if="apartment">
-      <!-- <figure>
-            <img class="cover-img" :src="`${BASE_URL_COVER_IMG}${apartment.cover_image}`" alt="">
-        </figure> -->
+    <header class="container">
+      <ul class="topbar">
+        <li class="topbar__item message-btn" @click="showModal">
+          <i>
+            <font-awesome-icon :icon="['fas', 'message']" />
+          </i>
+          <p>
+            Message
+          </p>
+        </li>
+        <li class="topbar__item share-btn">
+          <i>
+            <font-awesome-icon :icon="['fas', 'share']" />
+          </i>
+          <p>
+            Share
+          </p>
+        </li>
+        <li class="topbar__item save-btn">
+          <i>
+            <font-awesome-icon :icon="['fas', 'heart']" />
+          </i>
+          <p>
+            Save
+          </p>
+        </li>
+      </ul>
+    </header>
+    <main class="container" v-if="apartment">
+      
       <div class="carousel">
         <Carousel :items="apartment" />
       </div>
-      <!-- <div class="container-images">
-            <img class="images" v-for="img in apartment.images " :key="img.id" :src="`${BASE_URL_IMAGES}${img.link}`">
-        </div> -->
+      <h2 class="title">{{ apartment.name }}</h2>
       <div class="apartment-info">
-        <h3>{{ apartment.name }}</h3>
         <p>{{ apartment.address }}, {{ apartment.country }}</p>
         <p>{{ apartment.description }}</p>
         <div class="services">
@@ -20,7 +43,16 @@
           </div>
         </div>
       </div>
-    </div>
+      <MessageModal @close-modal="closeModal" v-show="isModalVisible" :items="apartment">
+        <template v-slot:header>
+          <h4>
+            Send a message to {{ apartment.user.first_name }}
+          </h4>
+        </template>
+        <template v-slot:body>   
+        </template>
+      </MessageModal>
+    </main >
     <Loading v-else></Loading>
   </DefaultLayout>
 </template>
@@ -30,12 +62,17 @@ import DefaultLayout from "../../layouts/DefaultLayout.vue";
 import Loading from "../../components/Loading.vue";
 import Carousel from "../../components/ApartmentImagesCarousel.vue";
 import store from "../../store";
+import { RouterLink } from "vue-router";
+import MessageModal from '../../components/MessageModal.vue';
 
 export default {
   components: {
     Loading,
     DefaultLayout,
     Carousel,
+    RouterLink,
+    MessageModal,
+
   },
   props: {
     slug: String,
@@ -48,6 +85,7 @@ export default {
       BASE_URL: "http://127.0.0.1:8000/api",
       BASE_URL_COVER_IMG: "http://127.0.0.1:8000/storage/",
       BASE_URL_IMAGES: `http://127.0.0.1:8000/storage/`,
+      isModalVisible: false,
     };
   },
   methods: {
@@ -56,6 +94,13 @@ export default {
         this.apartment = res.data.apartment;
       });
     },
+    closeModal() {
+      this.isModalVisible = false
+      // console.log('click')
+    },
+    showModal() {
+      this.isModalVisible = true
+    }
   },
   created() {
     this.fetchApartment();
@@ -69,49 +114,82 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+@use "../../styles/partials/variables" as *;
+
 .container {
-  max-width: 90%;
-  margin: 0 auto;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .topbar {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    order: -2;
+    padding: 10px 30px;
+    gap: 8px;
+    background-color: $primary;
+    flex-shrink: 0;
+    height: 50px;
+    font-weight: bold;
+
+    .message-btn {
+      flex-grow: 1;
+
+    }
+    .topbar__item:hover > i {
+      
+        font-size: 20px;
+    }
+    .topbar__item {
+      display: flex;
+      gap: 4px;
+      align-items: center;
+    }
+  }
 
   .carousel {
     overflow-x: hidden;
     max-width: 100%;
-    padding: 20px 0;
+    background-color: $light-grey;
+    margin-bottom: 20px;
   }
-}
 
-.container-images {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-}
+  .title::first-letter {
+    text-transform: uppercase;
 
-.cover-img {
-  display: block;
-  width: 100%;
-  margin: 16px 0;
-}
-
-.images {
-  max-width: 150px;
-  display: block;
-}
-
-.services {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-}
-
-.col-2 {
-  flex-basis: calc(100% / 6);
-}
-
-.apartment-info {
-  padding: 12px;
-
-  * {
-    margin: 8px 0;
   }
-}
-</style>
+
+  .title {
+    
+    text-align: center;
+    margin-bottom: 8px;
+
+  }
+
+  .apartment-info {
+    padding: 0 30px;
+  }
+
+
+  @media (min-width: 576px) {
+    .topbar {
+      margin-bottom: 8px;
+    }
+    .title {
+      order: -1;
+    }
+  }
+
+
+  @media (min-width: 768px) {}
+
+
+  @media (min-width: 992px) {}
+
+
+  @media (min-width: 1200px) {}
+
+
+  @media (min-width: 1400px) {}
+}</style>
